44a217d0637c6fd69a95302ab1cc6f52b3348d35
Arman Bhimani <Armanbhimani@gmail.com>
2019-04-22 18:27:40 -0500

    First Commiut

---
+++ src/validator.js
@@ -0,0 +1,16 @@
+const { API_TOKEN } = require('./config');
+const logger = require('./logger')
+
+function validateBearerToken(req, res, next) {
+    const authToken = req.get('Authorization')
+
+    logger.error(`Unauthorized request to path: ${req.path}`)
+
+    if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
+        return res.status(401).json({error: 'Unauthorized access'})
+    }
+
+    next();
+}
+
+module.exports = validateBearerToken;
